25ff8c4a7958b337ca5a5bcdf16da5db1dc68363
ArtemYanin <63047827+ArtemYanin@users.noreply.github.com>
2020-08-22 19:47:40 +1200

    Updates

---
+++ precache-manifest.77988f1617b638ca1a36e61f5301dc2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0030e69489bc898d032c0f711208e36",
+    "url": "/SkillfactoryProjectReactJS/index.html"
+  },
+  {
+    "revision": "a2b2ffe20b247b94a575",
+    "url": "/SkillfactoryProjectReactJS/static/css/main.8f471ad5.chunk.css"
+  },
+  {
+    "revision": "b398dca3af9ab9a349e7",
+    "url": "/SkillfactoryProjectReactJS/static/js/2.ed38ebb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SkillfactoryProjectReactJS/static/js/2.ed38ebb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b2ffe20b247b94a575",
+    "url": "/SkillfactoryProjectReactJS/static/js/main.d804b0b7.chunk.js"
+  },
+  {
+    "revision": "e423591765d61adfe916",
+    "url": "/SkillfactoryProjectReactJS/static/js/runtime-main.6dd9e841.js"
+  },
+  {
+    "revision": "21197298b88f8a0bb99960d7a97ec049",
+    "url": "/SkillfactoryProjectReactJS/static/media/cross.21197298.png"
+  },
+  {
+    "revision": "fe068a570b603c452cf3358891a69121",
+    "url": "/SkillfactoryProjectReactJS/static/media/userMenuDown.fe068a57.svg"
+  },
+  {
+    "revision": "3627788a2e512654cb62d046883c98bc",
+    "url": "/SkillfactoryProjectReactJS/static/media/userMenuUp.3627788a.svg"
+  }
+]);
